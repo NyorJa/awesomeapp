dadf64f7e6b0a4a8ec853599a493640fae7a9307
Rod Fetalvero <botsot.felix@gmail.com>
2023-05-07 22:24:22 +0800

    Http calls

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert' as convert;
 
 class Loading extends StatefulWidget {
-  const Loading({Key? key}) : super(key: key);
-
   @override
   State<Loading> createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async {
+    Uri uri = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
+    Response response = await get(uri);
+    if(response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+      print(jsonResponse);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Text('loading screen'),
+    );
   }
 }
